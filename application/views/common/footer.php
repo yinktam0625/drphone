9e0059fd1aca6765b7cb6deef3b288673e7870ff
yinktam0625 <yinktam0625@icloud.com>
2017-05-23 01:53:05 +0800

    Completed initial setup

---
+++ application/views/common/footer.php
@@ -0,0 +1,9 @@
+        </div>
+        <link href="<?php echo base_url('bootstrap/css/boostrap.min.css'); ?>" rel="stylesheet">
+        
+        <!-- Bootstrap core JavaScript-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script src="<?php echo base_url('bootstrap/js/boostrap.min.js'); ?>"></script>
+        <script src="<?php echo base_url('bootstrap/js/docs.min.js'); ?>"></script>
+    </body>
+</html>
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,7 +1,28 @@
 <!DOCTYPE html>
 
 <html>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name=""
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="<?php echo base_url('bootstrap/ico/favicon.ico'); ?>">
+        <title><?php echo $this->lang->line('system_system_name'); ?></title>
+
+        <!-- Boostrap core CSS -->
+        <link href="<?php echo base_url('bootstrap/css/bootstrap.min.css');?>" rel="stylesheet">
+        <!-- Boostrap theme -->
+        <link href="<?php echo base_url('bootstrap/css/bootstrap-theme.min.css');?>" rel="stylesheet">
+        <!-- Custom styles for thie template -->
+        <link href="<?php echo base_url('bootstrap/css/theme.css');?>" rel="stylesheet">
+
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    
+    <body role="document">
+        <div class="container theme-showcase" role="main">
